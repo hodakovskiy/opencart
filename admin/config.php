ad7841e937ac5f6d211307f5c569929084a93993
Sergey Hodakovskiy <hodakovskiy@gmail.com>
2018-06-27 19:07:02 +0300

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,26 +1,34 @@
 <?php
+
+// Get Base URI
+$baseUri = str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname($_SERVER['SCRIPT_FILENAME']));
+$http = str_replace(array('/', '\\'), '/', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $baseUri . '/');
+$catalog = explode('/', $http);
+array_pop($catalog);
+array_pop($catalog);
+$catalog = implode('/', $catalog) . '/';
 // HTTP
-define('HTTP_SERVER', 'http://localhost/opencart/admin/');
-define('HTTP_CATALOG', 'http://localhost/opencart/');
-define('HTTP_IMAGE', 'http://localhost/opencart/image/');
-
+define('HTTP_SERVER', $http);
+define('HTTP_CATALOG', $catalog);
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
-define('HTTPS_CATALOG', 'http://localhost/opencart/');
-define('HTTPS_IMAGE', 'http://localhost/opencart/image/');
-
+define('HTTPS_SERVER', $http);
+define('HTTPS_CATALOG', $catalog);
 // DIR
-define('DIR_APPLICATION', 'D:\xampp\htdocs\opencart/admin/');
-define('DIR_SYSTEM', 'D:\xampp\htdocs\opencart/system/');
-define('DIR_DATABASE', 'D:\xampp\htdocs\opencart/system/database/');
-define('DIR_LANGUAGE', 'D:\xampp\htdocs\opencart/admin/language/');
-define('DIR_TEMPLATE', 'D:\xampp\htdocs\opencart/admin/view/template/');
-define('DIR_CONFIG', 'D:\xampp\htdocs\opencart/system/config/');
-define('DIR_IMAGE', 'D:\xampp\htdocs\opencart/image/');
-define('DIR_CACHE', 'D:\xampp\htdocs\opencart/system/cache/');
-define('DIR_DOWNLOAD', 'D:\xampp\htdocs\opencart/download/');
-define('DIR_LOGS', 'D:\xampp\htdocs\opencart/system/logs/');
-define('DIR_CATALOG', 'D:\xampp\htdocs\opencart/catalog/');
+define('OP_ADMIN', __DIR__);
+define('OP_ROOT', dirname(OP_ADMIN));
+define('DIR_APPLICATION', OP_ROOT . '/admin/');
+define('DIR_SYSTEM',      OP_ROOT . '/system/');
+define('DIR_LANGUAGE',    OP_ROOT . '/admin/language/');
+define('DIR_TEMPLATE',    OP_ROOT . '/admin/view/template/');
+define('DIR_CONFIG',      OP_ROOT . '/system/config/');
+define('DIR_IMAGE',       OP_ROOT . '/image/');
+define('DIR_CACHE',       OP_ROOT . '/system/cache/');
+define('DIR_DOWNLOAD',    OP_ROOT . '/system/download/');
+define('DIR_UPLOAD',      OP_ROOT . '/system/upload/');
+define('DIR_LOGS',        OP_ROOT . '/system/logs/');
+define('DIR_MODIFICATION', OP_ROOT . '/system/modification/');
+define('DIR_CATALOG',     OP_ROOT . '/catalog/');
+
 
 // DB
 define('DB_DRIVER', 'mysql');
